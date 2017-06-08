9f27b8a0c64d9dd64ab0b5348c59bd08979da189
weizhang210142 <weizhang210142@sohu-inc.com>
2017-06-08 20:23:28 +0800

    修改打印类，添加对匿名内部类的判断

--- library/src/main/java/com/mcmo/z/library/module/logger/LoggerPrint.java
+++ library/src/main/java/com/mcmo/z/library/module/logger/LoggerPrint.java
@@ -1,39 +1,57 @@
 package com.mcmo.z.library.module.logger;
 
+import android.util.Log;
+
 /**
  * Created by ZhangWei on 2017/5/22.
  */
 
 public class LoggerPrint {
-    /**
-     * 代码定位
-     *
-     * @param type
-     */
-    private static String printLocation(char type, String... msg) {
-        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
-        int i = 0;
+
+    public static String getLogString(String msg) {
+        StackTraceElement stackTraceElement[] = Thread.currentThread().getStackTrace();
+        int index = 0;
         //获取代码所运行的位置
-        for (StackTraceElement e : stack) {
+        for (StackTraceElement e : stackTraceElement) {
             String name = e.getClassName();
             if (!name.equals(LoggerPrint.class.getName())) {
-                i++;
+                index++;
             } else {
                 break;
             }
         }
         //进行方法位置偏移,这个和你的打印方法到系统的打印方法间的方法数有关。
-        i += 3;
+        index += 2;
         //当我能准确获取到I时，本部分已经完结，以下代码都是废话，请不要关注
-        String className = stack[i].getFileName();
-        String methodName = stack[i].getMethodName();
-        int lineNumber = stack[i].getLineNumber();
-        StringBuilder sb = new StringBuilder();
-        sb.append("   (").append(className).append(":").append(lineNumber).append(")# ").append(methodName);
+        String fullClassName = stackTraceElement[index].getClassName();
+        String className = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
+        if (className.contains("$"))
+            className = className.substring(0, className.lastIndexOf("$"));//处理匿名内部类
+        String methodName = stackTraceElement[index].getMethodName();
+        String lineNumber = String.valueOf(stackTraceElement[index].getLineNumber());
         //  代码定位格式：
         //（类名：代码行）
         //使用这种格式，打印出的内容就可以点击跳转代码
-        return sb.toString();
+        return "(" + className + ".java:" + lineNumber + ")   " + msg;
     }
 
+    public static void e(String tag, String msg) {
+        Log.e(tag, getLogString(msg));
+    }
+
+    public static void d(String tag, String msg) {
+        Log.d(tag, getLogString(msg));
+    }
+
+    public static void i(String tag, String msg) {
+        Log.i(tag, getLogString(msg));
+    }
+
+    public static void w(String tag, String msg) {
+        Log.w(tag, getLogString(msg));
+    }
+
+    public static void v(String tag, String msg) {
+        Log.v(tag, getLogString(msg));
+    }
 }
